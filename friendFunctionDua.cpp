cc12992b752f719b40b82bfcf146c5eda3843ed9
tari2025 <denilestari272@gmail.com>
2025-05-05 11:36:33 +0700

    friendFunctionDuan

---
+++ friendFunctionDua.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia;
+{
+public:
+    void showNilaiPelajar(pelajar & x);
+    // void showNilaiPelajar &x);
+};
+
+class pelajar
+{
+private:
+int main()
+
+    public : pelajar()
+    {
+        nilai = 100;
+    }
+};
+}
