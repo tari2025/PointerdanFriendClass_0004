1d7caf9465ff35566319ef11f76ae1af4e31f5cf
tari2025 <denilestari272@gmail.com>
2025-05-05 10:42:21 +0700

    class mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    void ShowNim()
+    {
+        cout << "No induk = " << nim << endl;
+    }
+} :
+
+    int main
